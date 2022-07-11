af3c7d5cef945b59cb6edaef046e4542af92fb33
sebas code <nahuelsebastiandiaz@icloud.com>
2022-07-11 13:08:10 +0000

    Order and notifiationns

--- icons/index.ts
+++ icons/index.ts
@@ -1,3 +1,4 @@
 export { Camera } from "./Camera";
 export { BackArrow } from "./BackArrow";
 export { Bell } from "./Bell";
+export { User } from "./User";
--- pages/CreateOrder.tsx
+++ pages/CreateOrder.tsx
@@ -1,13 +1,17 @@
-import { Header, Logo, Searchbar, Wrapper } from "../components";
+import { Header, Logo, Message, Searchbar } from "../components";
 import { searchProductByName } from "../helpers/product";
 
 export const CreateOrder = () => {
   return (
-    <Wrapper className="flex flex-col justify-between">
+    <section>
       <Header />
+      <Message
+        img="./icons/burger.png"
+        title="1. Agrega tus productos"
+        message="Elije los productos que deseas incluir en tu orden."
+      />
       <Searchbar searchInDatabase={searchProductByName} />
-      <p>hol</p>
       <Logo small />
-    </Wrapper>
+    </section>
   );
 };
